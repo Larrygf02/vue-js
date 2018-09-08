b41d1ab403e9eb3170c89beae2d03a8dc129cab8
Raul Larriega <raulgf_02@hotmail.com>
2018-09-08 12:34:39 -0500

    Host scopped

---
+++ workflow/host-module/src/App.vue
@@ -0,0 +1,21 @@
+<!-- Hot reload mantiene el estado en cada interaccion
+  y en cada tiempo
+-->
+<template>
+  <main>
+      <mensaje></mensaje>
+      <div>
+       <p>Probando el scoped</p>
+      </div>
+  </main>
+</template>
+
+<script>
+import mensaje from './components/mensaje.vue';
+export default {
+  components: {mensaje},
+}
+</script>
+
+<style>
+</style>
---
+++ workflow/host-module/src/components/mensaje.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p v-text="mensaje"></p>
+    </div>    
+</template>
+<script>
+export default {
+    data(){
+        return {
+            mensaje: 'Hola que talco'
+        }
+    }
+}
+</script>
+<!--El scoped hace que tus estilos solo sean en tu componente,
+ se maneja como un shadow DOM -->
+<style scoped>
+    div p{
+        color: red;
+    }
+</style>
