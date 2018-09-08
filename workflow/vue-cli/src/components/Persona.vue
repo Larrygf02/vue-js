54de5435d8b03a60851e6c821476ebb7c44ba864
Raul Larriega <raulgf_02@hotmail.com>
2018-09-08 09:59:03 -0500

    Single file components

---
+++ workflow/vue-cli/src/components/Persona.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <template v-if="persona">
+            <h1 v-text="datosPersona.nombre"></h1>
+            <h2 v-text="datosPersona.correo"></h2>
+            <img :src="datosPersona.foto" alt="">
+        </template>
+        <span v-else>
+            Cargando Persona
+        </span>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    mounted() {
+        axios.get('https://randomuser.me/api/')
+            .then(response => {
+                this.persona = response.data.results[0];
+            })
+    },
+    data() {
+        return {
+            persona: null,
+        }
+    },
+    computed: {
+        datosPersona(){
+            return {
+                nombre: `${this.persona.name.first} ${this.persona.name.last}`,
+                foto: `${this.persona.picture.large}`,
+                correo: `${this.persona.email}`,
+            }
+        }
+
+    }
+}
+</script>
+<style>
+
+</style>
+
