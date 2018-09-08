dc2d56b665ca510f1ab66eee9495e6a515f51c8c
Raul Larriega <raulgf_02@hotmail.com>
2018-09-08 11:26:47 -0500

    Render

---
+++ workflow/render/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <elemento tipo="footer">Hola mundo</elemento>
+</template>
+
+<script>
+import elemento from './components/Elemento.vue';
+export default {
+  components: {elemento},
+}
+</script>
+
+<style>
+
+</style>
