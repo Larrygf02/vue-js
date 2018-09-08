96e9b711ac70ea9ff823659df0e5799466807e83
Raul Larriega <raulgf_02@hotmail.com>
2018-09-08 07:50:03 -0500

    Vuex

---
+++ manejo-estado/vuex-state/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <contador :cantidad="cantidad" @aumentar="cantidad++" @reducir="cantidad--">
+    </contador>
+</template>
+
+<script>
+import Contador from './components/contador.vue';
+export default {
+    components: {Contador},
+    data() {
+        return {
+            cantidad: 0,
+        }
+    }
+}
+</script>
---
+++ manejo-estado/vuex-state/components/contador.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Contador: {{ cantidad }}</h1>
+        <button @click="aumentar">Aumentar</button>
+        <button @click="reducir">Reducir</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['cantidad'],
+    methods: {
+        aumentar() {
+            this.$emit('aumentar');
+        },
+        reducir() {
+            this.$emit('reducir');
+        }
+    }
+}
+</script>
