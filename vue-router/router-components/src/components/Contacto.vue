fec53fb7dd4a3fcca648bdd9c9b737e78f63e75b
Raul Larriega <raulgf_02@hotmail.com>
2018-09-30 01:44:48 -0500

    Programing navigation

--- vue-router/router-components/src/components/Contacto.vue
+++ vue-router/router-components/src/components/Contacto.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        Contacto
-    </div>
+    <section>
+        <h2>Contacto</h2>
+        <button @click="volver()">Volver al home</button>
+    </section>
 </template>
+<script>
+export default {
+    methods: {
+        volver: function(){
+            this.$router.push('/', () => console.log('Ruta cambiada')); //Retorna al home
+        }
+    }
+}
+</script>
+
