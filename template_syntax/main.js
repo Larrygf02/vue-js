4360073acc417c4204d333747e5672f74a587681
Larrygf02 <raulgf_02@hotmail.com>
2019-03-26 16:32:19 -0500

    style in console log

--- template_syntax/main.js
+++ template_syntax/main.js
@@ -7,5 +7,10 @@ var directives = new Vue({
         attributename: "href",
         url: 'https://vuejs.org/v2/guide/syntax.html#Dynamic-Arguments'
     },
+    created: function() {
+        console.log(`%c esan-backoffice `,
+                    'background: #DD0332; padding:1px; border-radius: 3px; color: #fff'
+         )
+    }
 
 });
